e90a75a38195211d51d1fc2fa93f4d93a1f62d4a
roman tsiupiak <romantsiupiak@gmail.com>
2024-10-08 23:01:55 +0300

    config: init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+
+export default [
+  {
+    ignores: ['dist/']
+  },
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}", "tests/**/*.{js,mjs,cjs,ts}"]
+  },
+  {
+    languageOptions: { globals: globals.node }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["tests/**/*.{js,mjs,cjs,ts}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off"
+    }
+  },
+  eslintPluginPrettierRecommended,
+];
