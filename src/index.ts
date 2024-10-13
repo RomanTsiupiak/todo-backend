90b03bcdb34652f5007ecf70ecd0b8dbc05b12ad
roman tsiupiak <romantsiupiak@gmail.com>
2024-10-13 22:28:15 +0300

    config: add cors config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import { config } from '@/config';
 import { todo } from '@/mocks/todos';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/todos', (_, res) => {
   res.json(todo);
