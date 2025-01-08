71778fdf2a6c9378b69c076895a417347e4d51b8
roman tsiupiak <romantsiupiak@gmail.com>
2025-01-08 23:50:41 +0200

    add express configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Start");
+import express from "express";
+
+import { config } from "./config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("todo res");
+});
+
+app.listen(config.port);
